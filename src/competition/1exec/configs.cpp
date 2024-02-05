5cfe3df46b6d640483e5436b90ba09a097c7f3dd
br3135 <144181191+br3135@users.noreply.github.com>
2024-02-04 21:53:25 -0600

    Update configs.cpp

--- src/competition/1exec/configs.cpp
+++ src/competition/1exec/configs.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-Driver robot(std::vector<int>{12, 13, 14}, std::vector<int>{-17, -18, -19},
+Driver robot(std::vector<int>{12, 13, 14}, std::vector<int>{-17, -20, -19},
              2.75, 600, 16, 8.7);
 
 DigitalSystem front_wings('D');
@@ -9,7 +9,7 @@ DigitalSystem hang1('C'), hang2('F');
 
 DigitalSystem left_wing('G'), right_wing('H');
 
-pros::Motor cata(-20);
+pros::Motor cata(-18);
 pros::Motor intake(15);
 
 pros::ADIDigitalIn auton_select('B');
