2778d2e534da70e1b24af1668f4a0e7baa92938d
kjaca321 <129645281+kjaca321@users.noreply.github.com>
2024-04-16 08:25:01 -0500

    DELETE speed-variable 1D trajectory gen

--- src/library/api/control/trajectory1d.cpp
+++ src/library/api/control/trajectory1d.cpp
@@ -39,43 +39,6 @@ Trajectory1D::Trajectory1D(float desired_dist) {
   }
 }
 
-Trajectory1D::Trajectory1D(float desired_dist,
-                           std::vector<math::Vector> speeds) {
-  total_distance = desired_dist;
-  acc = global_max_acceleration, vel0 = global_min_velocity,
-  vels = global_max_velocity, dist = total_distance;
-  float initial_factor = (dist * acc + vel0 * vel0) / (2 * vels) + vels / 2;
-  float factor =
-      (dist > 25) ? (initial_factor - 5)
-                  : ((dist > 10) ? (initial_factor - 10) : initial_factor - 15);
-
-  std::vector<math::Vector> compounded_speeds = {};
-  float compounded_dist = 0;
-  for (math::Vector i : speeds) {
-    compounded_dist += i.x;
-    compounded_speeds.push_back(math::Vector(compounded_dist, i.y));
-  }
-
-  internal_max_vel = std::min(vels, factor);
-  vel = internal_max_vel;
-
-  float dist_trav = 0;
-  float time = 0;
-  trajectory = {};
-  while (dist_trav <= total_distance) {
-    for (math::Vector i : compounded_speeds)
-      if (dist_trav <= i.x) {
-        internal_max_vel = std::min({vels, i.y, factor});
-        break;
-      }
-    vel = internal_max_vel;
-    math::Pose1D kinematics = get_kinematics(dist_trav);
-    trajectory.push_back(kinematics);
-    dist_trav += kinematics.vel * dt;
-    time += dt;
-  }
-}
-
 std::vector<math::Pose1D> Trajectory1D::get() { return trajectory; }
 
 math::Pose1D Trajectory1D::get_kinematics(float distance_travelled) {
